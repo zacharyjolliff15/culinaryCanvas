45aaf2df52696fa00b21ae7681c818323911592d
Zachary <zacharyjolliff15@gmail.com>
2024-03-10 04:05:21 -0400

    stable before new ingredient

--- public/javascripts/ingredients.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const ingredientsList = document.getElementById('ingredientsList');
-    ingredientsData.forEach(row => {
-        const li = document.createElement('li');
-        li.textContent = row.ingredients; // Assuming 'ingredients' is the property name
-        ingredientsList.appendChild(li);
-    });
-});
