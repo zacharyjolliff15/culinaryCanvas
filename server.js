d32bb4c4fe4e1b1d040c5a7dee0ddd77b90c2f9a
Zachary <zacharyjolliff15@gmail.com>
2024-03-10 04:38:30 -0400

    working post data for ingredients

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const connection = mysql.createConnection({
   database: 'CulinaryCanvas'
 });
 
+app.use(express.urlencoded({ extended: true })); // Add this line to parse URL-encoded bodies
+
+
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to database:', err);
@@ -37,6 +40,18 @@ app.get('/ingredients', (req, res) => {
   });
 });
 
+app.post('/ingredients', (req, res) => {
+  const { ingredientName, units } = req.body;
+  const sql = 'INSERT INTO ingredients (ingredients, units_unit_id) VALUES (?, ?)';
+  connection.query(sql, [ingredientName, units], (err, result) => {
+      if (err) {
+          console.error('Error executing query:', err);
+          res.status(500).send('Error saving the ingredient');
+          return;
+      }
+      res.send('Ingredient added successfully');
+  });
+});
 
 
 // Serve static files from the 'public' directory
